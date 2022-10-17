757932de10d658e1b71776748fdeaabce5c12b0a
Samael Pitrofski <54420821+samaelpitrofski@users.noreply.github.com>
2022-10-17 17:50:43 -0300

    Integrando o firebase auth com o login
    Integrando o firebase auth com o login

---
+++ Login/index.js
@@ -0,0 +1,53 @@
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+      window.location.href = "home.html";
+  }
+})
+
+var formSignin = document.querySelector('#signin')
+var formSignup = document.querySelector('#signup')
+var btnColor = document.querySelector('.btnColor')
+
+document.querySelector('#btnSignin')
+  .addEventListener('click', () => {
+    formSignin.style.left = "25px"
+    formSignup.style.left = "450px"
+    btnColor.style.left = "0px"
+})
+
+document.querySelector('#btnSignup')
+  .addEventListener('click', () => {
+    formSignin.style.left = "-450px"
+    formSignup.style.left = "25px"
+    btnColor.style.left = "110px"
+})
+
+function login() {
+  firebase.auth().signInWithEmailAndPassword(
+    form.email().value, form.password().value
+  ).then(() => {
+      window.location.href = "home.html";
+  }).catch(error => {
+      alert(getErrorMessage(error));
+  });
+}
+
+function teste(){
+  alert(form.email().value + form.password().value);
+}
+
+function getErrorMessage(error) {
+  if (error.code == "auth/user-not-found") {
+      return "Usuário nao encontrado";
+  }
+  if (error.code == "auth/wrong-password") {
+      return "Senha inválida";
+  }
+  return error.message;
+}
+
+const form = {
+  email: () => document.getElementById("email"),
+  loginButton: () => document.getElementById("login-button"),
+  password: () => document.getElementById("password"),
+} 
