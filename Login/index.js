9f3ce32684f7338a9694e66a1b6434908a0a9436
Samael Pitrofski <54420821+samaelpitrofski@users.noreply.github.com>
2022-10-21 13:37:18 -0300

    Validações ao registrar user
    Nessa branch foram implementadas algumas validações para o cadastro de novos usuarios.

--- Login/index.js
+++ Login/index.js
@@ -6,7 +6,7 @@ firebase.auth().onAuthStateChanged(user => {
 })
 
 
-// CONTROLA AS ABAS DE CADASTRO E LOGIN
+// CONTROLA A NAVEGAÇÃO ENTRE ABAS DE CADASTRO E LOGIN
 var formSignin = document.querySelector('#signin')
 var formSignup = document.querySelector('#signup')
 var btnColor = document.querySelector('.btnColor')
@@ -41,17 +41,78 @@ function teste(){
   alert(form.email().value + form.password().value);
 }
 
+
+// validando novos users
+let regemail = document.querySelector('#regemail')
+let labelEmail = document.querySelector('#labelEmail')
+let validEmail = false
+
+let password = document.querySelector('#regpassword1')
+let labePassword1 = document.querySelector('#labelPassword1')
+let validPassword1 = false
+
+let confirmPassword = document.querySelector('#regpassword2')
+let labePassword2 = document.querySelector('#labelPassword2')
+let validPassword2 = false
+
+regemail.addEventListener('keyup', () => {
+  if(regemail.value.length <= 2){
+    labelEmail.setAttribute('style', 'color: red')
+    labelEmail.innerHTML = 'Insira no minimo 3 caracteres'
+    regemail.setAttribute('style', 'border-color: red')
+    validEmail = false
+  } else {
+    labelEmail.setAttribute('style', 'color: green')
+    labelEmail.innerHTML = 'Email'
+    regemail.setAttribute('style', 'border-color: green')
+    validEmail = true
+  }
+})
+
+password.addEventListener('keyup', () => {
+  if(password.value.length <= 5){
+    labePassword1.setAttribute('style', 'color: red')
+    labePassword1.innerHTML = 'Insira no minimo 6 caracteres'
+    password.setAttribute('style', 'border-color: red')
+    validPassword1 = false
+  } else {
+    labePassword1.setAttribute('style', 'color: green')
+    labePassword1.innerHTML = 'Senha'
+    password.setAttribute('style', 'border-color: green')
+    validPassword1 = true
+  }
+})
+
+confirmPassword.addEventListener('keyup', () => {
+  if(password.value != confirmPassword.value){
+    labePassword2.setAttribute('style', 'color: red')
+    labePassword2.innerHTML = 'As senhas não conferem'
+    confirmPassword.setAttribute('style', 'border-color: red')
+    validPassword2 = false
+  } else {
+    labePassword2.setAttribute('style', 'color: green')
+    labePassword2.innerHTML = 'Confirmar Senha'
+    confirmPassword.setAttribute('style', 'border-color: green')
+    validPassword2 = true
+  }
+})
+
 // FUNCTION DE CADASTRAR NOVO USER
 function register() {
   const regemail = form.regemail().value;
   const regpassword = form.regpassword1().value;
-  firebase.auth().createUserWithEmailAndPassword(
-      regemail, regpassword
-  ).then(() => {
-      window.location.href = "home.html";
-  }).catch(error => {
-      alert(getErrorMessage(error));
-  })
+
+  if(validEmail && validPassword1 && validPassword2){
+    firebase.auth().createUserWithEmailAndPassword(
+        regemail, regpassword
+    ).then(() => {
+        window.location.href = "home.html";
+    }).catch(error => {
+        alert(getErrorMessage(error));
+    })
+  } else {
+    alert("deu ruim gurizada");
+  }
 }
 
 
@@ -64,6 +125,7 @@ function getErrorMessage(error) {
   if (error.code == "auth/wrong-password") {
       return "Senha inválida";
   }
+
   return error.message;
 }
 
