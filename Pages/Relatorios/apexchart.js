9dfe1292fb79f4fb0a145b4597f4099fca0a6856
Samael Pitrofski <54420821+samaelpitrofski@users.noreply.github.com>
2022-11-23 17:00:09 -0300

    Update apexchart.js

--- Pages/Relatorios/apexchart.js
+++ Pages/Relatorios/apexchart.js
@@ -31,8 +31,8 @@ util.qsa('.date-carousel').forEach(function(el){ new DateCarousel(el) });
 
 //pegando a troca de data do carousel  
 
-document.getElementById("dataprev").addEventListener("click", charts);
-document.getElementById("datanext").addEventListener("click", charts);
+document.getElementById("dataprev").addEventListener("click", attchart);
+document.getElementById("datanext").addEventListener("click", attchart);
 
 
 
@@ -70,15 +70,20 @@ function getWithIndex(url){
 
 //graficos
 
+function attchart(){
+  attchart1();
+  attchart3();
+}
 
-
-
-function charts(){
+function attchart1(){
 
   datanext();
 
-  // gráfico 1 (linha) - saída
-  var fahrenheit = getWithIndex("http://localhost:3001/chartsaida?id_user="+iduser+"&mes="+mes+"&ano="+ano)
+  ;
+  var chart = new ApexCharts(document.querySelector("#spark1"), options);
+  chart.render();
+
+  var fahrenheit = getWithIndex("http://localhost:3001/chartsaida?id_user="+iduser+"&mes="+mes+"&ano="+ano);
 
 
   let values_operation = [];
@@ -88,217 +93,277 @@ function charts(){
     values_operation.push(elem.preco_operation)
   });
 
+  chart.updateSeries([
+    {
+      data: values_operation
+    }
+  ]);
 
-  var data_operacoes = fahrenheit.map(function(elem){
-      // console.log((elem.datas_saida).split("T")[0].split("-").reverse().join("/"));
-      date_operations.push((elem.datas_saida).split("T")[0].split("-").reverse().join("/"));
-  });
+}
 
-  var valores_operacoes = fahrenheit.map(function(elem){
-      values_operation.push(elem.preco_operation)
-  });
+function attchart2(){
 
-  // gráfico 3 (linha) - entrada
-  var fahrenheit_enter = getWithIndex("http://localhost:3001/chartentrada?id_user="+iduser+"&mes="+mes+"&ano="+ano)
-  // console.log(fahrenheit_enter)
+  datanext();
+  var chart = new ApexCharts(document.querySelector("#spark3"), options);
+  chart.render();
 
-  let values_operation_enter = [];
-  let date_operations_enter = [];
+var pizza_exit = getWithIndex("http://localhost:3001/chartcategorysaida?id_user="+iduser+"&mes="+mes+"&ano="+ano);
+// console.log(pizza_exit)
 
-  var data_operacoes_enter = fahrenheit_enter.map(function(elem){
-    // console.log((elem.datas_saida).split("T")[0].split("-").reverse().join("/"));
-    date_operations_enter.push((elem.datas_saida).split("T")[0].split("-").reverse().join("/"));
-  });
+let values_operation_pizza_exit = [];
+let category_operations_pizza_exit = [];
 
-  var valores_operacoes_enter = fahrenheit_enter.map(function(elem){
-    values_operation_enter.push(elem.preco_operation.toFixed())
-  });
+var categoria_operacoes_pizza_exit = pizza_exit.map(function(elem){
+  category_operations_pizza_exit.push((elem.category));
+});
 
-  // gráfico 2 (pizza) - saída
-  var pizza_exit = getWithIndex("http://localhost:3001/chartcategorysaida?id_user="+iduser+"&mes="+mes+"&ano="+ano)
-  // console.log(pizza_exit)
+var valores_operacoes_pizza_exit = pizza_exit.map(function(elem){
+  values_operation_pizza_exit.push(elem.qtd)
 
-  let values_operation_pizza_exit = [];
-  let category_operations_pizza_exit = [];
+  chart.updateSeries([
+    {
+      data: values_operation_pizza_exit
+    }
+  ]);
 
-  var categoria_operacoes_pizza_exit = pizza_exit.map(function(elem){
-    category_operations_pizza_exit.push((elem.category));
-  });
+})}
 
-  var valores_operacoes_pizza_exit = pizza_exit.map(function(elem){
-    values_operation_pizza_exit.push(elem.qtd)
-  });
 
-  // gráfico 4 (pizza) - entrada
-  var pizza_enter = getWithIndex("http://localhost:3001/chartcategoryentrada?id_user="+iduser+"&mes="+mes+"&ano="+ano)
-  // console.log(pizza_enter)
+function attchart3(){
 
-  let values_operation_pizza_enter = [];
-  let category_operations_pizza_enter = [];
+  datanext();
+  var chart = new ApexCharts(document.querySelector("#spark3"), options);
+  chart.render();
 
-  var categoria_operacoes_pizza_enter = pizza_enter.map(function(elem){
-    category_operations_pizza_enter.push((elem.category));
-  });
+var fahrenheit_enter = getWithIndex("http://localhost:3001/chartentrada?id_user="+iduser+"&mes="+mes+"&ano="+ano)
+// console.log(fahrenheit_enter)
 
-  var valores_operacoes_pizza_enter = pizza_enter.map(function(elem){
-    values_operation_pizza_enter.push(elem.qtd)
-  });
+let values_operation_enter = [];
+let date_operations_enter = [];
 
-  var options = {
-    series: [{
-    name: "Gasto",
-    data: values_operation
-  }],
-      chart: {
-      // height: 400,
-      // width: 600,
-      type: 'line',
-      zoom: {
-      enabled: false
-      }
-  },
-  dataLabels: {
-      enabled: false
-  },
-  stroke: {
-      curve: 'straight'
-  },
-  title: {
-      text: 'Gastos por dia',
-      align: 'left'
-  },
-  grid: {
-      row: {
-      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-      opacity: 0.5
-      },
-  },
-  xaxis: {
-      categories: date_operations,
-  },
-  responsive: [
-      {
-        breakpoint: 1000,
-        options: {
-          plotOptions: {
-            bar: {
-              horizontal: false
-            }
-          },
-          legend: {
-            position: "bottom"
-          }
-        }
-      }
-    ]
-  };
+var data_operacoes_enter = fahrenheit_enter.map(function(elem){
+  // console.log((elem.datas_saida).split("T")[0].split("-").reverse().join("/"));
+  date_operations_enter.push((elem.datas_saida).split("T")[0].split("-").reverse().join("/"));
+});
 
-  var options2 = {
-    series: values_operation_pizza_exit,
-    chart: {
-    width: 380,
-    type: 'pie',
-  },
-  labels: category_operations_pizza_exit,
-  responsive: [{
-    breakpoint: 480,
-    options: {
-      chart: {
+var valores_operacoes_enter = fahrenheit_enter.map(function(elem){
+  values_operation_enter.push(elem.preco_operation.toFixed())
+});
+
+  chart.updateSeries([
+    {
+      data: values_operation_enter,
+    }
+  ]);
+
+}
+
+
+datanext();
+// gráfico 1 (linha) - saída
+var fahrenheit = getWithIndex('http://localhost:3001/chartsaida?id_user=Oz6u4xnMCNZqDhNBTwVLU7rjFOS2&mes=11&ano=2022')
+let values_operation = [];
+let date_operations = [];
+var data_operacoes = fahrenheit.map(function(elem){
+    console.log((elem.datas_saida).split("T")[0].split("-").reverse().join("/"));
+    date_operations.push((elem.datas_saida).split("T")[0].split("-").reverse().join("/"));
+});
+var valores_operacoes = fahrenheit.map(function(elem){
+    values_operation.push(elem.preco_operation)
+});
+// gráfico 3 (linha) - entrada
+var fahrenheit_enter = getWithIndex('http://localhost:3001/chartentrada?id_user=Oz6u4xnMCNZqDhNBTwVLU7rjFOS2&mes=11&ano=2022')
+console.log(fahrenheit_enter)
+let values_operation_enter = [];
+let date_operations_enter = [];
+var data_operacoes_enter = fahrenheit_enter.map(function(elem){
+  console.log((elem.datas_saida).split("T")[0].split("-").reverse().join("/"));
+  date_operations_enter.push((elem.datas_saida).split("T")[0].split("-").reverse().join("/"));
+});
+
+var valores_operacoes_enter = fahrenheit_enter.map(function(elem){
+  values_operation_enter.push(elem.preco_operation)
+  values_operation_enter.push(elem.preco_operation.toFixed())
+});
+
+// gráfico 2 (pizza) - saída
+var pizza_exit = getWithIndex('http://localhost:3001/chartcategorysaida?id_user=Oz6u4xnMCNZqDhNBTwVLU7rjFOS2&mes=11&ano=2022')
+console.log(pizza_exit)
+let values_operation_pizza_exit = [];
+let category_operations_pizza_exit = [];
+var categoria_operacoes_pizza_exit = pizza_exit.map(function(elem){
+  category_operations_pizza_exit.push((elem.category));
+});
+var valores_operacoes_pizza_exit = pizza_exit.map(function(elem){
+  values_operation_pizza_exit.push(elem.qtd)
+});
+// gráfico 4 (pizza) - entrada
+var pizza_enter = getWithIndex('http://localhost:3001/chartcategoryentrada?id_user=Oz6u4xnMCNZqDhNBTwVLU7rjFOS2&mes=11&ano=2022')
+console.log(pizza_enter)
+let values_operation_pizza_enter = [];
+let category_operations_pizza_enter = [];
+var categoria_operacoes_pizza_enter = pizza_enter.map(function(elem){
+  category_operations_pizza_enter.push((elem.category));
+});
+var valores_operacoes_pizza_enter = pizza_enter.map(function(elem){
+  values_operation_pizza_enter.push(elem.qtd)
+});
+var options = {
+        series: [{
+        name: "Gasto",
+        data: values_operation
+    }],
+        chart: {
         // height: 400,
         // width: 600,
         type: 'line',
         zoom: {
         enabled: false
-        }},
-      legend: {
-        position: 'bottom'
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    stroke: {
+        curve: 'straight'
+    },
+    title: {
+        text: 'Gastos por dia',
+        align: 'left'
+    },
+    grid: {
+        row: {
+        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5
+        },
+    },
+    xaxis: {
+        categories: date_operations,
+    },
+    responsive: [
+        {
+          breakpoint: 1000,
+          options: {
+            plotOptions: {
+              bar: {
+                horizontal: false
+              }
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+    var options2 = {
+      series: values_operation_pizza_exit,
+      chart: {
+      width: 380,
+      type: 'pie',
+    },
+    labels: category_operations_pizza_exit,
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          // height: 400,
+          // width: 600,
+          type: 'line',
+          zoom: {
+          enabled: false
+          }},
+        legend: {
+          position: 'bottom'
+        }
       }
-    }
-  }]
-  };
+    }]
+    };
 
-  var chart = new ApexCharts(document.querySelector("#spark1"), options);
-  var chart2 = new ApexCharts(document.querySelector("#spark2"), options2);
+  // mais gráficos
+    var options3 = {
+        series: [{
+        name: "Entrada",
+        data: values_operation_enter
+    }],
+        chart: {
+        // height: 400,
+        // width: 600,
+        type: 'line',
+        zoom: {
+        enabled: false
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    stroke: {
+        curve: 'straight'
+    },
+    title: {
+        text: 'Entradas por dia',
+        align: 'left'
+    },
+    grid: {
+        row: {
+        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5
+        },
+    },
+    xaxis: {
+        categories: date_operations_enter,
+    },
+    responsive: [
+        {
+          breakpoint: 1000,
+          options: {
+            plotOptions: {
+              bar: {
+                horizontal: false
+              }
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+    var options4 = {
+      series: values_operation_pizza_enter,
+      chart: {
+      width: 380,
+      type: 'pie',
+    },
+    labels: category_operations_pizza_enter,
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          // height: 400,
+          // width: 600,
+          type: 'line',
+          zoom: {
+          enabled: false
+          }},
+        legend: {
+          position: 'bottom'
+        }
+      }
+    }]
+};
+
+
+  function charts(){
+
+  datanext();
+  let chart = new ApexCharts(document.querySelector("#spark1"), options);
+  let chart2 = new ApexCharts(document.querySelector("#spark2"), options2);
   chart.render();
   chart2.render();
 
-  // mais gráficos
-
-  var options3 = {
-      series: [{
-      name: "Entrada",
-      data: values_operation_enter
-  }],
-      chart: {
-      // height: 400,
-      // width: 600,
-      type: 'line',
-      zoom: {
-      enabled: false
-      }
-  },
-  dataLabels: {
-      enabled: false
-  },
-  stroke: {
-      curve: 'straight'
-  },
-  title: {
-      text: 'Entradas por dia',
-      align: 'left'
-  },
-  grid: {
-      row: {
-      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-      opacity: 0.5
-      },
-  },
-  xaxis: {
-      categories: date_operations_enter,
-  },
-  responsive: [
-      {
-        breakpoint: 1000,
-        options: {
-          plotOptions: {
-            bar: {
-              horizontal: false
-            }
-          },
-          legend: {
-            position: "bottom"
-          }
-        }
-      }
-    ]
-  };
-
-  var options4 = {
-    series: values_operation_pizza_enter,
-    chart: {
-    width: 380,
-    type: 'pie',
-  },
-  labels: category_operations_pizza_enter,
-  responsive: [{
-    breakpoint: 480,
-    options: {
-      chart: {
-        // height: 400,
-        // width: 600,
-        type: 'line',
-        zoom: {
-        enabled: false
-        }},
-      legend: {
-        position: 'bottom'
-      }
-    }
-  }]
-  };
-
-  var chart3 = new ApexCharts(document.querySelector("#spark3"), options3);
-  var chart4 = new ApexCharts(document.querySelector("#spark4"), options4);
+  let chart3 = new ApexCharts(document.querySelector("#spark3"), options3);
+  let chart4 = new ApexCharts(document.querySelector("#spark4"), options4);
   chart3.render();
   chart4.render();
 
